3624fa326889f91565c5f08b2c502e598761e740
Geoff Howland <geoff@gmail.com>
2017-03-14 08:47:51 +0800

    Populating data files with initial words

---
+++ mandarin.py
@@ -0,0 +1,12 @@
+import yaml
+
+NOUNS = yaml.load(open('data/conjunction.yaml'))
+VERBS = yaml.load(open('data/conjunction.yaml'))
+ADJECTIVES = yaml.load(open('data/adjective.yaml'))
+QUESTIONS = yaml.load(open('data/question.yaml'))
+NUMBERS = yaml.load(open('data/conjunction.yaml'))
+COUNTER = yaml.load(open('data/conjunction.yaml'))
+CONJUNCTIONS = yaml.load(open('data/conjunction.yaml'))
+SPECIAL = yaml.load(open('data/special.yaml'))
+
+
