e1983d41d1aa4929df97fa05a62d1f064a220a15
Geoff Howland <geoff@gmail.com>
2017-03-17 00:26:32 +0800

    Update

--- mandarin.py
+++ mandarin.py
@@ -10,6 +10,20 @@ CONJUNCTIONS = yaml.load(open('data/conjunction.yaml'))
 SPECIAL = yaml.load(open('data/special.yaml'))
 
 
+# Assertion
+SENTENCE_STRUCTURES_1 = '%(subject) shir4 %(object)s'
+# Negative Assertion
+SENTENCE_STRUCTURES_2 = '%(subject) bu2_shir4 %(object)s'
+
+# Action sentence
+SENTENCE_STRUCTURES_3 = '%(subject) %(action)s %(object)s'
+
+# Description sentence
+SENTENCE_STRUCTURES_4 = '%(subject) %(adjective)s'
+
+
+
+
 def Main():
     pass
 
