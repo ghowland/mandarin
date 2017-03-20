32318b9815f1d581a65d334e980b51768b91a17c
Geoff Howland <geoff@gmail.com>
2017-03-20 18:16:47 +0800

    Update

--- mandarin.py
+++ mandarin.py
@@ -1,13 +1,25 @@
-import yaml
+#!/usr/bin/end python
 
-NOUNS = yaml.load(open('data/conjunction.yaml'))
-VERBS = yaml.load(open('data/conjunction.yaml'))
-ADJECTIVES = yaml.load(open('data/adjective.yaml'))
-QUESTIONS = yaml.load(open('data/question.yaml'))
-NUMBERS = yaml.load(open('data/conjunction.yaml'))
-COUNTER = yaml.load(open('data/conjunction.yaml'))
-CONJUNCTIONS = yaml.load(open('data/conjunction.yaml'))
-SPECIAL = yaml.load(open('data/special.yaml'))
+"""
+Mandarin
+"""
+
+
+import yaml
+import glob
+import os
+
+
+DATA_PATH_GLOB = 'data/*.yaml'
+
+# NOUNS = yaml.load(open('data/conjunction.yaml'))
+# VERBS = yaml.load(open('data/conjunction.yaml'))
+# ADJECTIVES = yaml.load(open('data/adjective.yaml'))
+# QUESTIONS = yaml.load(open('data/question.yaml'))
+# NUMBERS = yaml.load(open('data/conjunction.yaml'))
+# COUNTER = yaml.load(open('data/conjunction.yaml'))
+# CONJUNCTIONS = yaml.load(open('data/conjunction.yaml'))
+# SPECIAL = yaml.load(open('data/special.yaml'))
 
 
 # Assertion
@@ -22,11 +34,34 @@ SENTENCE_STRUCTURES_3 = '%(subject) %(action)s %(object)s'
 SENTENCE_STRUCTURES_4 = '%(subject) %(adjective)s'
 
 
+def LoadData():
+    """Returns a dict with the name of the file as the first key, and the reverse as 'reverse_*' and name"""
+    data = {}
+    
+    paths = glob.glob(DATA_PATH_GLOB)
+    
+    for path in paths:
+        key = os.path.basename(path).split('.')[0]
+        reverse_key = 'reverse_%s' % key
+        
+        data[key] = {}
+        data[reverse_key] = {}
+        
+        path_data = yaml.load(open(path))
+        
+        for (mandarin, english) in path_data.items():
+            data[key][mandarin] = english
+            
+            data[reverse_key][english] = mandarin
+    
+    return data
 
 
 def Main():
-    pass
+    data = LoadData()
 
 
 if __name__ == '__main__':
     Main()
+
+
