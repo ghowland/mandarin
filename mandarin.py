53a2eceb050ea3a38befc3425062ef12304ff814
Geoff Howland <geoff@gmail.com>
2017-03-20 18:38:46 +0800

    Update

--- mandarin.py
+++ mandarin.py
@@ -13,15 +13,6 @@ import pprint
 
 DATA_PATH_GLOB = 'data/*.yaml'
 
-# NOUNS = yaml.load(open('data/conjunction.yaml'))
-# VERBS = yaml.load(open('data/conjunction.yaml'))
-# ADJECTIVES = yaml.load(open('data/adjective.yaml'))
-# QUESTIONS = yaml.load(open('data/question.yaml'))
-# NUMBERS = yaml.load(open('data/conjunction.yaml'))
-# COUNTER = yaml.load(open('data/conjunction.yaml'))
-# CONJUNCTIONS = yaml.load(open('data/conjunction.yaml'))
-# SPECIAL = yaml.load(open('data/special.yaml'))
-
 
 # Assertion
 SENTENCE_STRUCTURES_1 = '%(subject) shir4 %(object)s'
@@ -35,48 +26,51 @@ SENTENCE_STRUCTURES_3 = '%(subject) %(action)s %(object)s'
 SENTENCE_STRUCTURES_4 = '%(subject) %(adjective)s'
 
 
-def LoadData():
-    """Returns a dict with the name of the file as the first key, and the reverse as 'reverse_*' and name"""
-    data = {}
+def LoadData(with_reverse=True):
+  """Returns a dict with the name of the file as the first key, and the reverse as 'reverse_*' and name"""
+  data = {}
+  
+  paths = glob.glob(DATA_PATH_GLOB)
+  
+  for path in paths:
+    key = os.path.basename(path).split('.')[0]
+    reverse_key = 'reverse_%s' % key
     
-    paths = glob.glob(DATA_PATH_GLOB)
+    data[key] = {}
     
-    for path in paths:
-        key = os.path.basename(path).split('.')[0]
-        reverse_key = 'reverse_%s' % key
-        
-        data[key] = {}
-        data[reverse_key] = {}
-        
-        path_data = yaml.load(open(path))
-        
-        for (mandarin, english) in path_data.items():
-            data[key][mandarin] = english
-            
-            if type(english) in (str, unicode):
-                data[reverse_key][english] = mandarin
-            elif type(english) in (int, float):
-                data[reverse_key][english] = str(mandarin)
-            else:
-                # print '%s:  %s = %s' % (key, mandarin, english)
-                
-                for item in english:
-                    if type(item) in (int, float):
-                        data[reverse_key][str(item)] = mandarin
-                        
-                    elif not item.startswith('*NOTE*') and not item.startswith('*NOUN*') and not item.startswith('*VERB*') and not item.startswith('*ADJ*'):
-                        data[reverse_key][item] = mandarin
+    if with_reverse:
+      data[reverse_key] = {}
     
-    return data
+    path_data = yaml.load(open(path))
+    
+    for (mandarin, english) in path_data.items():
+      data[key][mandarin] = english
+      
+      if with_reverse:
+        if type(english) in (str, unicode):
+          data[reverse_key][english] = mandarin
+        elif type(english) in (int, float):
+          data[reverse_key][english] = str(mandarin)
+        else:
+          # print '%s:  %s = %s' % (key, mandarin, english)
+          
+          for item in english:
+            if type(item) in (int, float):
+              data[reverse_key][str(item)] = mandarin
+              
+            elif not item.startswith('*NOTE*') and not item.startswith('*NOUN*') and not item.startswith('*VERB*') and not item.startswith('*ADJ*'):
+              data[reverse_key][item] = mandarin
+    
+  return data
 
 
 def Main():
-    data = LoadData()
-    
-    pprint.pprint(data)
+  data = LoadData(with_reverse=False)
+  
+  pprint.pprint(data)
 
 
 if __name__ == '__main__':
-    Main()
+  Main()
 
 
